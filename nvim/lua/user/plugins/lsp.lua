faa7916a00a1f711e489fdeaa1180009ec5251e3
Gilad Ben-Avi <gilad.benavi@appsflyer.com>
2023-08-14 19:38:40 +0300

    Remove init.vim

--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -38,12 +38,7 @@ function M.config()
   local config = {
     -- disable virtual text
     virtual_text = false,
-    -- show signs
-    signs = {
-      active = signs,
-    },
     update_in_insert = true,
-    underline = true,
     severity_sort = true,
     float = {
       focusable = false,
