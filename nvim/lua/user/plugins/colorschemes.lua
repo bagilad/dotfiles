07cebac845eb9d18dcfecf0178c16292a0e2a446
Gilad Ben-Avi <gilad.benavi@appsflyer.com>
2023-08-17 14:14:46 +0300

    Add nvim catppuccin theme

--- nvim/lua/user/plugins/colorschemes.lua
+++ nvim/lua/user/plugins/colorschemes.lua
@@ -4,20 +4,33 @@ local tokyonight = {
       lazy = false,    -- make sure we load this during startup if it is your main colorscheme
       priority = 1000, -- make sure to load this before all the other start plugins
     }
+tokyonight.opt = {
+  style = "storm", -- `storm`, `moon`, a darker variant `night` and `day`
+}
 -- The actual colorscheme that will be used if this theme is selected
 tokyonight.colorscheme = "tokyonight"
 
 local onedark = { "joshdick/onedark.vim" }
 onedark.colorscheme = "onedark"
 
+local catppuccin = { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+catppuccin.opt = {
+  flavour = "macchiato", -- latte, frappe, macchiato, mocha
+}
+catppuccin.colorscheme = "catppuccin"
+
 -- The selected theme
-local M = tokyonight
+local M = catppuccin
 
 function M.config()
+  if M.opt ~= nil then
+    require(M.colorscheme).setup(M.opt)
+  end
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.colorscheme)
   if not status_ok then
     return
   end
+
 end
 
 return M
