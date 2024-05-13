13e632a0c23bfea9a5c5bf44902cfce9d04118c5
Gilad Ben-Avi <bagilad@gmail.com>
2024-05-13 16:20:34 +0300

    Nvim plugins: eyeliner, neotab, neotest

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,13 +11,21 @@ spec "user.lspconfig"
 spec "user.cmp"
 spec "user.telescope"
 spec "user.none-ls"
+-- automatically highlighting other uses of the word under the cursor
 spec "user.illuminate"
 spec "user.whichkey"
 spec "user.nvimtree"
 spec "user.lualine"
+-- statusline/winbar component that uses LSP to show current code context
 spec "user.navic"
+-- works with nvim-navic to provide context about code in the winbar
 spec "user.breadcrumbs"
 spec "user.oil"
+spec "user.neotest"
+-- smooth scrolling
 spec "user.extras.neoscroll"
 spec "user.extras.harpoon"
+spec "user.extras.eyeliner"
+-- tabbing in and out of brackets, parentheses, quotes, and more
+spec "user.extras.neotab"
 require "user.lazy"
