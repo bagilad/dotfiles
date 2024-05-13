b0e0d52f786494bb22316ddf01c607a8ff025a48
Gilad Ben-Avi <bagilad@gmail.com>
2024-05-13 17:13:47 +0300

    Nvim plugin: gitsigns

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,22 +11,24 @@ spec "user.lspconfig"
 spec "user.cmp"
 spec "user.telescope"
 spec "user.none-ls"
--- automatically highlighting other uses of the word under the cursor
+-- Automatically highlighting other uses of the word under the cursor
 spec "user.illuminate"
+spec "user.gitsigns"
 spec "user.whichkey"
 spec "user.nvimtree"
 spec "user.lualine"
--- statusline/winbar component that uses LSP to show current code context
+-- Statusline/winbar component that uses LSP to show current code context
 spec "user.navic"
--- works with nvim-navic to provide context about code in the winbar
+-- Works with nvim-navic to provide context about code in the winbar
 spec "user.breadcrumbs"
 spec "user.oil"
 spec "user.neotest"
--- smooth scrolling
+-- Smooth scrolling
 spec "user.extras.neoscroll"
 -- Getting you where you want with the fewest keystrokes
 spec "user.extras.harpoon"
+-- Move faster with unique f/F indicators for each word on the line
 spec "user.extras.eyeliner"
--- tabbing in and out of brackets, parentheses, quotes, and more
+-- Tabbing in and out of brackets, parentheses, quotes, and more
 spec "user.extras.neotab"
 require "user.lazy"
--- nvim/lua/user/extras/harpoon.lua
+++ nvim/lua/user/extras/harpoon.lua
@@ -11,12 +11,8 @@ function M.config()
   local keymap = vim.keymap.set
   local opts = { noremap = true, silent = true }
   local harpoon = require("harpoon")
-  local function mark_file()
-    harpoon:list():add()
-    vim.notify "󱡅  marked file"
-  end
 
-  keymap("n", "<s-m>", mark_file, opts)
+  keymap("n", "<s-m>", function() harpoon:list():add() end, opts)
   keymap("n", "<TAB>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, opts)
 end
 
