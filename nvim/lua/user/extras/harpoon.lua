cec153bf072f260b5360281e3954efeb0fd51429
Gilad Ben-Avi <bagilad@gmail.com>
2024-05-14 18:03:10 +0300

    Vim: use Telescope as a UI in Harpoon

--- nvim/lua/user/extras/harpoon.lua
+++ nvim/lua/user/extras/harpoon.lua
@@ -4,16 +4,43 @@ local M = {
   event = "VeryLazy",
   dependencies = {
     { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim" },
   },
 }
 
 function M.config()
   local keymap = vim.keymap.set
   local opts = { noremap = true, silent = true }
-  local harpoon = require("harpoon")
+  local harpoon = require "harpoon"
+  harpoon:setup()
 
-  keymap("n", "<s-m>", function() harpoon:list():add() end, opts)
-  keymap("n", "<TAB>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, opts)
+  -- basic telescope configuration
+  local conf = require("telescope.config").values
+  local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+      table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers")
+      .new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table {
+          results = file_paths,
+        },
+        previewer = conf.file_previewer {},
+        sorter = conf.generic_sorter {},
+      })
+      :find()
+  end
+
+  keymap("n", "<s-m>", function()
+    harpoon:list():add()
+  end, opts)
+  opts.desc = "Open harpoon window"
+  keymap("n", "<TAB>", function()
+    toggle_telescope(harpoon:list())
+  end, opts)
 end
 
 return M
