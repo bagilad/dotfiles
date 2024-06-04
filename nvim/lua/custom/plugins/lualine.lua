e451cd199cb6757d5668e5cf84d09c97daa6f010
Gilad Ben-Avi <bagilad@gmail.com>
2024-06-04 17:36:04 +0300

    Vim: lualine defaults

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -1,27 +1,9 @@
-local M = {
-"nvim-lualine/lualine.nvim",
--- dependencies = {
---   "AndreM222/copilot-lualine",
--- },
-}
-
-function M.config()
-require("lualine").setup {
-  options = {
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    ignore_focus = { "NvimTree" },
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup {}
+    end,
   },
-  sections = {
-    lualine_a = {},
-    lualine_b = { "branch" },
-    lualine_c = { "diagnostics" },
-    -- lualine_x = { "copilot", "filetype" },
-    lualine_y = { "progress" },
-    lualine_z = {},
-  },
-  extensions = { "quickfix", "man", "fugitive" },
 }
-end
-
-return M
