da2b3e4a95b455d12a893b546c377ee18c86a4a4
Gilad Ben-Avi <bagilad@gmail.com>
2024-06-13 19:45:56 +0300

    Fish: starship prompt

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@
 # https://github.com/fish-shell/fish-shell/blob/master/share/config.fish
 # https://github.com/fish-shell/fish-shell/blob/da32b6c172dcfe54c9dc4f19e46f35680fc8a91a/share/config.fish#L257-L269
 
-set fish_greeting ""
+starship init fish | source
+set fish_greeting
 source ~/.asdf/asdf.fish
 
