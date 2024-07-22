5d489c7902ebb0a9704e47508132f34b9a6d890b
Gilad Ben-Avi <bagilad@gmail.com>
2024-07-22 11:02:12 +0300

    Nvim: upgrade whichkey

--- nvim/lua/config/whichkey.lua
+++ nvim/lua/config/whichkey.lua
@@ -1,28 +1,21 @@
-local mappings = {
-  ["g"] = { name = "+goto" },
-  ["]"] = { name = "+next" },
-  ["["] = { name = "+prev" },
-  ["<leader>"] = {
-    v = { "<cmd>vsplit<CR>", "vertical split" },
-    b = { name = "+buffer" },
-    d = { name = "+debug" },
-    s = { name = "+search" },
-    g = { name = "+git" },
-    -- gh = { name = "+hunks", ["_"] = "which_key_ignore" },
-    l = { name = "+LSP", ["_"] = "which_key_ignore" },
-    p = { name = "+plugins" },
-    t = { name = "+toggle", ["_"] = "which_key_ignore" },
-    o = { name = "+open" },
-    T = { name = "+test" },
-    w = { name = "+window" },
-    x = { name = "+diagnostics/quickfix" },
-  },
+local wk = require "which-key"
+
+wk.add {
+  { "<leader>T", group = "test" },
+  { "<leader>b", group = "buffer" },
+  { "<leader>d", group = "debug" },
+  { "<leader>g", group = "git" },
+  { "<leader>l", group = "LSP" },
+  { "<leader>l_", hidden = true },
+  { "<leader>o", group = "open" },
+  { "<leader>p", group = "plugins" },
+  { "<leader>s", group = "search" },
+  { "<leader>t", group = "toggle" },
+  { "<leader>t_", hidden = true },
+  { "<leader>v", "<cmd>vsplit<CR>", desc = "vertical split" },
+  { "<leader>w", group = "window" },
+  { "<leader>x", group = "diagnostics/quickfix" },
+  { "[", group = "prev" },
+  { "]", group = "next" },
+  { "g", group = "goto" },
 }
-
-local which_key = require "which-key"
-
-local opts = {
-  mode = "n", -- NORMAL mode
-}
-
-which_key.register(mappings, opts)
--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -9,19 +9,16 @@ function M.config()
   local harpoon = require "harpoon"
   harpoon:setup()
 
-  local opts = { noremap = true, silent = true }
   vim.keymap.set("n", "<s-m>", function()
     harpoon:list():add()
-  end, opts)
+  end)
 
-  opts.desc = "Open harpoon window"
   vim.keymap.set("n", "<TAB>", function()
     harpoon.ui:toggle_quick_menu(harpoon:list())
-  end, opts)
+  end, { desc = "Open harpoon window" })
 
   -- Set <space>1..<space>5 be my shortcuts to moving to the files
   for _, idx in ipairs { 1, 2, 3, 4, 5 } do
-    opts.desc = "Move to file " .. idx
     vim.keymap.set("n", string.format("<space>%d", idx), function()
       harpoon:list():select(idx)
     end, { desc = "which_key_ignore" })
